6cb05d5918ad1629fe0596a6be160a8e8a1efc78
David Rothfarb <david@dr809.dfci.harvard.edu>
2019-05-17 15:17:25 -0400

    Testing out different hostname options to get GA working

--- api/index.php
+++ api/index.php
@@ -1,5 +1,5 @@
 <?php
-  header("Access-Control-Allow-Origin: http://localhost:3000");
+  header("Access-Control-Allow-Origin: http://bcda.dr809.test");
   header("Access-Control-Allow-Credentials: true");
   header("Access-Control-Allow-Methods: GET, PUT, POST, DELETE");
   header("Access-Control-Max-Age: 1000");
