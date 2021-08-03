ce813998405ff45415d62efabbde1fe8f0a0833d
Dave Rothfarb <dave.rothfarb.webdev@gmail.com>
2021-08-03 18:51:40 -0400

    Changing api to https

--- api/index.php
+++ api/index.php
@@ -13,7 +13,7 @@
  * encryption function.
  */
   //header("Access-Control-Allow-Origin: http://localhost:3000");
-  header("Access-Control-Allow-Origin: http://consyderdecisiontool.org");
+  header("Access-Control-Allow-Origin: https://consyderdecisiontool.org");
   header("Access-Control-Allow-Credentials: true");
   header("Access-Control-Allow-Methods: GET, PUT, POST, DELETE");
   header("Access-Control-Max-Age: 1000");
